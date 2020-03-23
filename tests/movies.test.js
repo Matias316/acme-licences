a7f027843fbd0eee8a96e12cda648feded94086e
Matias <mjaindu@hotmail.com>
2020-03-23 15:50:17 -0300

    Add test cases for Get, update, delete for movies and songs

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -4,6 +4,43 @@ const request = supertest(app);
 
 describe('Movies endpoints', () => {
 
+  it('Test PUT - GET by Id - Update - Delete movie', async () => {
+    //Test PUT
+    const movieCreatedResponse = await request.post('/api/movies')
+    .send({
+      "title": "Forrest Gump",
+      "genre": "Comedy"
+      });
+
+      expect(movieCreatedResponse.status).toBe(201)
+      expect(movieCreatedResponse.body.movie).toHaveProperty('id')
+      expect(movieCreatedResponse.body.movie.genre).toBe('Comedy');
+
+    //Test UPDATE
+    const movieGetResponse = await request.get(`/api/movies/${movieCreatedResponse.body.movie.id}`);
+
+    expect(movieGetResponse.status).toBe(200)
+    expect(movieGetResponse.body.movie.title).toBe('Forrest Gump');
+
+    //Test UPDATE
+    const movieUpdateResponse = await request.put(`/api/movies/${movieGetResponse.body.movie.id}`)
+    .send({
+      "title": "New title",
+      "genre": "New genre"
+    });
+  
+    expect(movieUpdateResponse.status).toBe(200)
+    expect(movieUpdateResponse.body.message).toBe('Movie updated successfully.');
+
+    //Test DELETE
+    const movieDeleteResponse = await request.delete(`/api/movies/${movieGetResponse.body.movie.id}`);
+   
+    expect(movieDeleteResponse.status).toBe(200)
+    expect(movieDeleteResponse.body.message).toBe('Movie deleted successfully.');
+
+  });
+
+  
   it('Test GET all movies', async () => {
         const response = await request.get('/api/movies');
         expect(response.status).toBe(200);
--- tests/songs.test.js
+++ tests/songs.test.js
@@ -4,18 +4,42 @@ const request = supertest(app);
 
 describe('Songs endpoints', () => {
   
-  it('Test PUT song', async () => {
-    const response = await request.post('/api/songs')
+  it('Test PUT - GET by Id - Update - Delete song', async () => {
+    //Test PUT
+    const songCreatedResponse = await request.post('/api/songs')
     .send({
       "title": "Back in Black",
       "duration": 100,
       "owner": "Sony Music"
       });
 
-      expect(response.status).toBe(201)
-      expect(response.body.song).toHaveProperty('id')
-      expect(response.body.song.title).toBe('Back in Black');
+      expect(songCreatedResponse.status).toBe(201)
+      expect(songCreatedResponse.body.song).toHaveProperty('id')
+      expect(songCreatedResponse.body.song.title).toBe('Back in Black');
     
+      //Test GET
+      const songGetResponse = await request.get(`/api/songs/${songCreatedResponse.body.song.id}`);
+
+      expect(songGetResponse.status).toBe(200)
+      expect(songGetResponse.body.song.title).toBe('Back in Black');
+
+      //Test UPDATE
+      const songUpdateResponse = await request.put(`/api/songs/${songGetResponse.body.song.id}`)
+      .send({
+        "title": "New title",
+        "duration": 200,
+        "owner": "New owner"
+        });
+  
+      expect(songUpdateResponse.status).toBe(200)
+      expect(songUpdateResponse.body.message).toBe('Song updated successfully.');
+
+       //Test DELETE
+       const songDeleteResponse = await request.delete(`/api/songs/${songGetResponse.body.song.id}`);
+   
+       expect(songDeleteResponse.status).toBe(200)
+       expect(songDeleteResponse.body.message).toBe('Song deleted successfully.');
+
   });
 
   it('Test GET all songs', async () => {
